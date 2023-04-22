7bbd40f51c42e9dd717cc68555536da95fb66e9b
上田貴央 <gibson511511@gmail.com>
2023-04-23 00:52:16 +0900

    copy paste

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
+import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -37,6 +40,9 @@ export default function Home() {
           height={37}
           priority
         />
+        <div className={styles.thirteen}>
+          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        </div>
       </div>
 
       <div className={styles.grid}>
@@ -46,22 +52,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p className={inter.className}>
+            Find in-depth information about Next.js features and API.
+          </p>
         </a>
 
         <a
@@ -70,10 +66,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <h2 className={inter.className}>
             Templates <span>-&gt;</span>
           </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
@@ -82,10 +78,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
+          <p className={inter.className}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
