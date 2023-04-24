4389e634333245ee8466acd8fc7d1573d00408a5
上田貴央 <gibson511511@gmail.com>
2023-04-25 08:28:19 +0900

    add articleDetails

--- app/pages/articles/[slug].tsx
+++ app/pages/articles/[slug].tsx
@@ -1,6 +1,5 @@
 import { notFound } from 'next/navigation';
 import { Article, Comment } from '@/components/types';
-import { Suspense } from 'react';
 
 const getArticle = async (slug: string) => {
 	const res = await fetch(`http://localhost:3000/api/articles/${slug}`, {
@@ -30,35 +29,25 @@ const getComments = async (slug: string) => {
 	}
 
 	const data = await res.json();
-	return data as Comment[];
+	return data.comments as Comment[];
 };
 
 export default async function ArticleDetail({ params }: { params: { slug: string } }) {
 	const articlePromise = getArticle(params.slug);
-	const commentPromise = getComments(params.slug);
+	const commentsPromise = getComments(params.slug);
 
-	const article = await articlePromise;
+	const [article, comments] = await Promise.all([articlePromise, commentsPromise]);
 
 	return (
 		<div>
 			<h1>{article.title}</h1>
 			<p>{article.content}</p>
 			<h2>Comments</h2>
-			<Suspense fallback={<div>Loading comments...</div>}>
-				{/* @ts-expect-error 現状は jsx が Promise を返すと TypeScript が型エラーを報告するが、将来的には解決される */}
-				<Comments commentPromise={commentPromise} />
-			</Suspense>
+			<ul>
+				{comments.map((comment) => (
+					<li key={comment.id}>{comment.body}</li>
+				))}
+			</ul>
 		</div>
-	);
-}
-
-async function Comments({ commentPromise }: { commentPromise: Promise<Comment[]> }) {
-	const comments = await commentPromise;
-	return (
-		<ul>
-			{comments.map((comment) => (
-				<li key={comment.id}>{comment.body}</li>
-			))}
-		</ul>
 	);
 }
